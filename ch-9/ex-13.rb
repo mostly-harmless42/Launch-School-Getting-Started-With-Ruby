8b5e2842012974fbfbbcb1b43838b4aa1b13b243
mostly-harmless42 <wrgread1@gmail.com>
2023-02-05 17:59:54 -0800

    Add chapter 9 exercises

---
+++ ch-9/ex-13.rb
@@ -0,0 +1,7 @@
+arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
+
+arr.delete_if { |str| str.start_with?('s') }
+
+arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
+
+arr.delete_if { |str| str.start_with?('s', 'w') }
---
+++ ch-9/ex-15.rb
@@ -0,0 +1,1 @@
+#These hashes are not the same!
